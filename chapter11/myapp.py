05900e5ae575419409670b13231834c8b2f8d947
danyow-cheung <76671016+danyow-cheung@users.noreply.github.com>
2023-06-14 07:49:05 +0800

    0614

---
+++ chapter11/myapp.py
@@ -0,0 +1,20 @@
+import time 
+def medium():
+    time.sleep(0.02)
+
+def light():
+    time.sleep(0.001)
+
+def heavy():
+    for i in range(100):
+        light()
+        medium()
+        medium()
+    time.sleep(2)
+def main():
+    for i in range(2):
+        heavy()
+
+if __name__ =='__main__':
+    main()
+    
